91290dfd4e39037d90b82851deb7a314580e0fc8
Kalle Kankaanpää <kalle.kankaanpaa@protonmail.com>
2022-08-30 22:32:14 +0300

    Fixed archaicly spelled pigeon

--- tests.ts
+++ tests.ts
@@ -2,14 +2,14 @@ import { assert } from "chai";
 import filterMap from "./index.js";
 
 describe('filterMap()', function () {
-  const animals = ["crow", "cat", "dog", "lion", "cow", "pidgeon"];
+  const animals = ["crow", "cat", "dog", "lion", "cow", "pigeon"];
   it('filters', function () {
     const result = filterMap(animals, (animal) => animal.length < 4, (animal) => animal);
     assert.deepEqual(result, ["cat", "dog", "cow"]);
   });
   it('maps', function () {
     const result = filterMap(animals, () => true, (animal) => animal.toUpperCase());
-    assert.deepEqual(result, ["CROW", "CAT", "DOG", "LION", "COW", "PIDGEON"]);
+    assert.deepEqual(result, ["CROW", "CAT", "DOG", "LION", "COW", "PIGEON"]);
   });
   it('filters and maps', function () {
     const result = filterMap(animals, (animal) => animal.length < 4, (animal) => animal.toUpperCase());
